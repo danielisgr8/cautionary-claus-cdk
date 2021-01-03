d608a3ff52dc1c529969453e3ef9b4cb88e307a4
danielisgr8 <schubertmdaniel@gmail.com>
2021-01-03 02:00:53 -0800

    Mostly implemented API stack

--- bin/cautionary-claus-cdk.ts
+++ bin/cautionary-claus-cdk.ts
@@ -3,7 +3,10 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { CautionaryClausAmplifyStack } from "../lib/amplify-stack";
 import { CautionaryClausCognitoStack } from "../lib/cognito-stack";
+import { CautionaryClausRestApiStack } from "../lib/rest-api-stack";
+import { buildId } from "../lib/util";
 
 const app = new cdk.App();
-new CautionaryClausAmplifyStack(app, "CautionaryClausAmplifyStack");
-new CautionaryClausCognitoStack(app, "CautionaryClausCognitoStack");
+new CautionaryClausAmplifyStack(app, buildId("AmplifyStack"));
+new CautionaryClausCognitoStack(app, buildId("CognitoStack"));
+new CautionaryClausRestApiStack(app, buildId("RestAPIStack"));
---
+++ lib/rest-api-stack.ts
@@ -0,0 +1,84 @@
+import * as cdk from "@aws-cdk/core";
+import * as apigateway from "@aws-cdk/aws-apigatewayv2";
+import * as apigatewayintegrations from "@aws-cdk/aws-apigatewayv2-integrations";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import { Duration } from "@aws-cdk/core";
+import { HttpMethod } from "@aws-cdk/aws-apigatewayv2";
+import { buildId } from "./util";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+
+export class CautionaryClausRestApiStack extends cdk.Stack {
+  private table: dynamodb.Table;
+  private defaultIntegration: apigatewayintegrations.LambdaProxyIntegration;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.buildTable();
+    this.buildLambda();
+    this.buildHttpApi();
+  }
+
+  private buildTable() {
+    this.table = new dynamodb.Table(this, buildId("ProfileTable"), {
+      partitionKey: { name: "username", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+  }
+
+  private buildLambda() {
+    const defaultLambdaFn = new lambda.Function(this, buildId("DefaultFn"), {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(""), // TODO: connect to lambda package/repo/etc.,
+      functionName: "Cautionary Claus Default Function",
+      initialPolicy: [new PolicyStatement({
+        resources: [this.table.tableArn],
+        actions: [
+          "dynamodb:BatchGetItem",
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ]
+      })]
+    });
+
+    this.defaultIntegration = new apigatewayintegrations.LambdaProxyIntegration({
+      handler: defaultLambdaFn,
+    });
+  }
+
+  private buildHttpApi() {
+    // TODO: cognito authorizer
+    const httpApi = new apigateway.HttpApi(this, buildId("RestAPI"), {
+      corsPreflight: {
+        allowHeaders: ["Authorization"],
+        allowMethods: [HttpMethod.GET, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST],
+        allowOrigins: ["*"],
+        maxAge: Duration.days(10),
+      }
+    });
+
+    // Routes documented in config/api-definition.yaml
+    httpApi.addRoutes({
+      path: "/profile/{username}",
+      methods: [HttpMethod.GET],
+      integration: this.defaultIntegration
+    });
+
+    httpApi.addRoutes({
+      path: "/profile/{username}/note",
+      methods: [HttpMethod.PUT, HttpMethod.DELETE],
+      integration: this.defaultIntegration
+    });
+
+    new apigateway.HttpStage(this, buildId("RestAPIStage"), {
+      httpApi,
+      stageName: "prod"
+    });
+  }
+}
